00a3932be0fc8e78ba70f04d0f6649d2537dd8fc
Hazar Artuner <hazar.artuner@gmail.com>
2019-04-25 13:45:59 +0300

    feat(Development): Add prettier and linter support

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.scss';
 
-export default class App extends Component {
-  render() {
-    return <div className="App__Component">
-      React App Component
-    </div>
-  }
-}
-
+export default () => <div className="App__Component">React App Component</div>;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from 'components/App';
 
-render(<App />, document.getElementById("app"));
+render(<App />, document.getElementById('app'));
 
 if (module.hot) {
   module.hot.accept('components/App', () => {
-    const App = require('components/App/index.js').default;
+    const HotApp = require('components/App/index.js').default; // eslint-disable-line global-require
 
-    render(<App />, document.getElementById("app"));
+    render(<HotApp />, document.getElementById('app'));
   });
 }
