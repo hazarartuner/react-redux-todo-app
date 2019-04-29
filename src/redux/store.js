1636306c30a74e194843f7cba2aa3038611e7266
Hazar Artuner <hazar.artuner@gmail.com>
2019-04-29 14:42:11 +0300

    fix(Redux): Fix store configuration method for production environment

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,12 +4,11 @@ import reducers from './reducer';
 
 export default function configureStore(initialState) {
   const { __REDUX_DEVTOOLS_EXTENSION__ } = window;
+  const enhancers = [];
 
-  const enhancers = [
-    process.env.NODE_ENV !== 'production' &&
-      __REDUX_DEVTOOLS_EXTENSION__ &&
-      __REDUX_DEVTOOLS_EXTENSION__(),
-  ];
+  if (process.env.NODE_ENV !== 'production' && __REDUX_DEVTOOLS_EXTENSION__) {
+    enhancers.push(__REDUX_DEVTOOLS_EXTENSION__());
+  }
 
   return createStore(reducers, initialState, ...enhancers);
 }
